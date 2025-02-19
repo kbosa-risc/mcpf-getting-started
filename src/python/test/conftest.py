8073dad9b87bfe7c2f22d2da83f0bb8af35051f7
Leo Savernik <leo.savernik@risc-software.at>
2025-02-19 13:41:56 +0100

    P0-233 Generalised pipeline execution in test cases
    Former-commit-id: 24f458e6418c5cf6c86a3cecf7b64f728e213051

---
+++ src/python/test/conftest.py
@@ -0,0 +1,11 @@
+from typing import Callable, Generator, List
+
+import pytest
+import mcp_frm.pipeline_runtime as runtime
+
+def _actually_run_pipeline(*args: str):
+    runtime.run(*args)
+
+@pytest.fixture
+def run_pipeline() -> Generator[Callable[..., None], None, None]:
+    return _actually_run_pipeline
